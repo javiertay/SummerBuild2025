ae00fc8aa5c8d1534cd4fb89313022f21d9e030c
ryanyipp <159006040+ryanyipp@users.noreply.github.com>
2025-06-04 22:01:34 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './components/login/Login'
-
 import Login from './components/Login/Login.jsx'
 
 function App() {
